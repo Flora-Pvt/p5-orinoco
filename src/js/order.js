5ef9e27728e19b2b03e2cdf74103d7816512eb1a
Flora-Pvt <f.preuvot@live.fr>
2020-09-14 18:14:48 -0500

    on order.html refresh go to index

--- src/js/order.js
+++ src/js/order.js
@@ -4,4 +4,10 @@ let orderId = localStorage.getItem('orderId');
 document.getElementById('total').innerHTML += total / 100;
 document.getElementById('orderId').innerHTML += orderId;
 
-//localStorage.clear();
+localStorage.clear();
+
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {  
+  window.location.href = "../index.html";
+}
+ 
+  
