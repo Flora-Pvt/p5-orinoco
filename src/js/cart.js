e64123c5afd4ae73353b85bae3f5117b469bd814
Flora-Pvt <f.preuvot@live.fr>
2020-09-17 12:20:03 -0500

    cart add, remove and clear products

--- src/js/cart.js
+++ src/js/cart.js
@@ -17,21 +17,71 @@ function displayCart() {
   console.log(localStorage.getItem('cart'));
   // pour chaque produit dans le panier affiche son image, son nom et son prix dans le tableau
   if (productsInCart !== null) {
-    for (i = 0; i < productsInCart.length; i++) {      
-      let clone = template.content.cloneNode(true);      
+    for (i = 0; i < productsInCart.length; i++) {
+      let clone = template.content.cloneNode(true);
       let img = clone.getElementById('img');
       img.setAttribute('src', productsInCart[i].imageUrl);
       let name = clone.getElementById('name');
-      let quantity = clone.getElementById('quantity');            
+      let quantity = clone.getElementById('quantity');
       let price = clone.getElementById('price');
-      
+      let removeBtn = clone.getElementById('remove');
+      let addBtn = clone.getElementById('add');
+      let clearBtn = clone.getElementById('clear');
+
+      removeBtn.dataset.id = productsInCart[i]._id;
       name.innerHTML = productsInCart[i].name;
       quantity.innerHTML = productsInCart[i].number;
       price.innerHTML += (productsInCart[i].number * productsInCart[i].price) / 100 + " €";
       template.parentNode.appendChild(clone);
+
+      removeBtn.addEventListener("click", () => {
+        console.log("enlève une quantité");
+        let id = removeBtn.dataset.id;
+        let ourProduct = productsInCart.filter(p => p._id == id);
+        ourProduct[0].number--;
+        let newCart = productsInCart.filter(p => p._id != id);
+        newCart.push(ourProduct[0]);
+        localStorage.setItem("cart", JSON.stringify(newCart));
+        quantity.innerHTML = ourProduct[0].number;
+        price.innerHTML = (ourProduct[0].number * ourProduct[0].price) / 100 + " €";
+        let total = 0;
+        // pour chaque produit dans le panier...
+        for (i = 0; i < productsInCart.length; i++) {
+          // ...ajoute le prix au total déclaré   
+          total = total + parseInt(productsInCart[i].number * productsInCart[i].price);
+        }
+        cartTotal.innerHTML = total / 100;
+        localStorage.setItem("total", total);
+      })
+      addBtn.addEventListener("click", () => {
+        console.log("ajoute une quantité");
+        let id = removeBtn.dataset.id;
+        let ourProduct = productsInCart.filter(p => p._id == id);
+        ourProduct[0].number++;
+        let newCart = productsInCart.filter(p => p._id != id);
+        newCart.push(ourProduct[0]);
+        localStorage.setItem("cart", JSON.stringify(newCart));
+        quantity.innerHTML = ourProduct[0].number;
+        price.innerHTML = (ourProduct[0].number * ourProduct[0].price) / 100 + " €";
+        let total = 0;
+        // pour chaque produit dans le panier...
+        for (i = 0; i < productsInCart.length; i++) {
+          // ...ajoute le prix au total déclaré   
+          total = total + parseInt(productsInCart[i].number * productsInCart[i].price);
+        }
+        cartTotal.innerHTML = total / 100;
+        localStorage.setItem("total", total);
+      })
+      clearBtn.addEventListener("click", () => {
+        console.log("supprime le produit");
+        let id = removeBtn.dataset.id;
+        let newCart = productsInCart.filter(p => p._id != id);        
+        localStorage.setItem("cart", JSON.stringify(newCart));        
+        location.reload();
+      })
     }
-  } else {    
-    document.getElementById("no-product").innerHTML = "Vous n'avez pas d'ourson dans le panier";    
+  } else {
+    document.getElementById("no-product").innerHTML = "Vous n'avez pas d'ourson dans le panier";
   }
 
 
@@ -47,7 +97,7 @@ function displayCart() {
   // affiche le total dans le html
   cartTotal.innerHTML = total / 100;
   localStorage.setItem("total", total);
- 
+
   /* --- envoie les id du panier et infos du formulaire au serveur au clic sur Terminer l'achat--- */
 
   orderBtn.addEventListener('click', () => {
@@ -78,15 +128,15 @@ function displayCart() {
 
       /* --- crée la fonction pour envoyer les données au serveur /order --- */
 
-      function post() { 
-        return new Promise((resolve, reject) => { 
-          let request = new XMLHttpRequest(); 
-          request.open("POST", 'http://localhost:3000/api/teddies/order'); 
-          request.setRequestHeader("content-type", "application/json"); 
-          request.send(JSON.stringify(data)); 
-          request.onreadystatechange = function () { 
-            if (this.readyState === 4) { 
-              if (this.status === 201) { 
+      function post() {
+        return new Promise((resolve, reject) => {
+          let request = new XMLHttpRequest();
+          request.open("POST", 'http://localhost:3000/api/teddies/order');
+          request.setRequestHeader("content-type", "application/json");
+          request.send(JSON.stringify(data));
+          request.onreadystatechange = function () {
+            if (this.readyState === 4) {
+              if (this.status === 201) {
                 resolve(JSON.parse(this.responseText));
               } else {
                 reject(request);
@@ -96,16 +146,16 @@ function displayCart() {
         })
       }
       post("http://localhost:3000/api/teddies/order", data)
-        .then(function (response) { 
-          let orderId = response.orderId; 
+        .then(function (response) {
+          let orderId = response.orderId;
           console.log(orderId);
-          localStorage.setItem("orderId", orderId);          
+          localStorage.setItem("orderId", orderId);
           window.location.href = "order.html"
         })
-        .catch(function () { 
+        .catch(function () {
           alert('error');
         });
-    }    
+    }
   });
 }
 
@@ -119,10 +169,10 @@ clearCartBtn.addEventListener("click", () => {
 
 /* --- afficher le nombre de produits dans le panier après chargement de la page --- */
 window.addEventListener("load", function () {
-  let inCart = JSON.parse(localStorage.getItem('cart'));  
+  let inCart = JSON.parse(localStorage.getItem('cart'));
   let quantityInCart = 0;
-  for (i = 0; i < inCart.length; i++) {        
-      quantityInCart += inCart[i].number;        
+  for (i = 0; i < inCart.length; i++) {
+    quantityInCart += inCart[i].number;
   }
   document.querySelector('.cart-items').innerHTML = quantityInCart;
 });
