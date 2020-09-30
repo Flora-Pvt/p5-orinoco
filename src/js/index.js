4d802598884f0819747ac5a13e991f5fab38fb25
Flora-Pvt <f.preuvot@live.fr>
2020-09-30 15:39:16 -0500

    POO implementation

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,52 @@
-/* --- afficher les produits --- */
-function displayProducts(products) {
-    for (i = 0; i < products.length; i++) {
-        // détermine où afficher les informations des produits dans le HTML
-        let clone = template.content.cloneNode(true);
-        let id = clone.getElementById('id');
-        let img = clone.getElementById('img');
-        let link = clone.getElementById('link');
-        let name = clone.getElementById('name');
-        let price = clone.getElementById('price');
-        // injecte les informations des produits dans le HTML
-        id.setAttribute('href', "pages/produit.html?id=" + products[i]._id);
-        img.setAttribute('src', products[i].imageUrl);
-        img.setAttribute('alt', "ours en peluche " + products[i].name);
-        link.setAttribute('href', "pages/produit.html?id=" + products[i]._id);
-        name.innerHTML = products[i].name;
-        price.innerHTML += products[i].price / 100;
-        template.parentNode.appendChild(clone);
-    }
+function main () {
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+  const productId = urlParams.get('id')
+
+  const inCart = JSON.parse(localStorage.getItem('cart'))
+  const index = document.getElementById('index')
+  const cartPage = document.getElementById('cart')
+  const orderId = localStorage.getItem('orderId')
+
+  const data = new DataSource()
+  const dom = new DOM()
+  const order = new Order()
+
+  /* --- fonctionnalités de la page index --- */
+  if (index) {
+    data
+      .getProducts()
+      .then(products => {
+        dom.displayProducts(products)
+        dom.displayCartNumber(inCart)
+      })
+  }
+
+  /* --- fonctionnalités de la page produit --- */
+  else if (productId) {
+    data
+      .getProduct(productId)
+      .then(products => {
+        dom.displayProduct(products)
+        dom.displayCartNumber(inCart)
+        order.addProduct(inCart, products, productId)
+      })
+  }
+
+  /* --- fonctionnalités de la page panier --- */
+  else if (cartPage) {
+    dom.displayCart(inCart)
+    dom.displayTotal(inCart)
+    dom.displayCartNumber(inCart)
+    order.operations(inCart)
+    order.formSubmit()
+    data.postData(inCart)
+  }
+
+  /* --- fonctionnalités de la page commande --- */
+  else if (orderId) {
+    dom.displayOrder(orderId)
+  }
 }
 
-/* --- demande les informations au serveur et appelle la fonction displayProducts --- */
-makeRequest(url);
-
-/* --- afficher le nombre de produits dans le panier à côté du logo --- */
-displayCartNumber();
-
+window.onload = main
--- src/js/order.js
+++ src/js/order.js
@@ -1,13 +1,106 @@
-let total = localStorage.getItem('total');
-let orderId = localStorage.getItem('orderId');
+class Order {
+  constructor () {
+    this.dom = new DOM()
+  }
 
-document.getElementById('total').innerHTML += total / 100;
-document.getElementById('orderId').innerHTML += orderId;
+  /* --- fonction réutilisable qui change la quantité du produit --- */
+  changeQuantity (inCart) {
+    const cartPage = document.getElementById('cart')
+    const id = event.target.dataset.id
+    const ourProduct = inCart.filter(p => p._id === id)
+    // si bouton "-" retire le produit
+    if (event.target.classList.contains('remove') && ourProduct[0].number > 1) {
+      ourProduct[0].number--
+      const quantity = event.target.nextElementSibling
+      quantity.innerHTML = ourProduct[0].number
+    }
+    // si bouton d'ajout, ajoute le produit
+    else if (event.target.classList.contains('add')) {
+      ourProduct[0].number++
+      const quantity = event.target.previousElementSibling
+      quantity.innerHTML = ourProduct[0].number
+    }
+    // enregistre le produit avec sa nouvelle quantité au panier
+    const newCart = inCart.filter(p => p._id !== id)
+    newCart.push(ourProduct[0])
+    localStorage.setItem('cart', JSON.stringify(newCart))
+    // affiche les nouveaux totaux et quantité obtenus
+    this.dom.displayCartNumber(inCart)
+    if (cartPage) {
+      this.dom.displayTotal(inCart)
+      this.dom.displayProductTotal(ourProduct)
+    }
+  }
 
-localStorage.clear();
+  /* --- page produit : ajoute un produit en fonction du panier --- */
+  addProduct (inCart, products, productId) {
+    const addBtn = document.getElementById('add')
+    const feedback = document.getElementById('feedback')
+    // enregistre le produit au panier (dans le stockage local)
+    addBtn.addEventListener('click', () => {
+      if (inCart && colors.validity.valid === true) {
+        // si déjà dans le panier met à jour sa quantité
+        const isPresent = inCart.some(p => p._id === productId)
+        if (isPresent) {
+          const order = new Order()
+          order.changeQuantity(inCart)
+          feedback.innerHTML = 'Quantité dans le panier : '
+        }
+        else {
+          // ajoute ce produit pour la première fois
+          products.number = 1
+          inCart.push(products)
+          localStorage.setItem('cart', JSON.stringify(inCart))
+          quantity.innerHTML = products.number
+          feedback.innerHTML = 'Quantité dans le panier : '
+          this.dom.displayCartNumber(inCart)
+        }
+      }
+      else if (colors.validity.valid === false) {
+        feedback.innerHTML = 'Veuillez choisir une couleur.'
+      }
+      else {
+        inCart = []
+        // ajoute un tout premier produit au stockage local
+        products.number = 1
+        inCart.push(products)
+        localStorage.setItem('cart', JSON.stringify(inCart))
+        quantity.innerHTML = products.number
+        feedback.innerHTML = 'Quantité dans le panier : '
+        console.log(this.dom, inCart)
+        this.dom.displayCartNumber(inCart) // /!\ ne marche pas
+      }
+    })
+  }
 
-if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {  
-  window.location.href = "../index.html";
+  /* --- page panier : supprime, ajoute ou enlève --- */
+  operations (inCart) {
+    const cartContent = document.querySelector('.cart-content')
+    cartContent.addEventListener('click', event => {
+      if (event.target.classList.contains('clear')) {
+        const id = event.target.dataset.id
+        const newCart = inCart.filter(p => p._id !== id)
+        localStorage.setItem('cart', JSON.stringify(newCart))
+        location.reload()
+      }
+      else if (event.target.classList.contains('add')) {
+        this.changeQuantity(inCart)
+      }
+      else if (event.target.classList.contains('remove')) {
+        this.changeQuantity(inCart)
+      }
+    })
+  }
+
+  /* --- affiche les aides pour le formulaire --- */
+  formSubmit () {
+    const formOrder = document.getElementById('form')
+    formOrder.addEventListener('submit', function (event) {
+      if (formOrder.checkValidity() === false) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      formOrder.classList.add('was-validated')
+    }, false)
+  }
 }
- 
-  
